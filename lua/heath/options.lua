c306f09663ab1da467f34739755450d72adc115f
heath <heath@havingfun.online>
2024-07-06 23:41:38 -0400

    playing with options

--- lua/heath/options.lua
+++ lua/heath/options.lua
@@ -5,7 +5,9 @@ vim.g.have_nerd_font = true
 vim.cmd("let g:netrw_liststyle = 3")
 
 local o = vim.o
-local opt = vim.opt
+
+-- fat cursor, still thinking this one over
+-- o.guicursor = ""
 
 o.showmode = false
 o.undofile = true
@@ -14,24 +16,28 @@ o.clipboard = "unnamedplus" -- use system clipboard as default register
 o.number = true
 o.relativenumber = true
 
-o.expandtab = true
-o.shiftwidth = 2
-o.smartindent = true
 o.tabstop = 2
 o.softtabstop = 2
+o.shiftwidth = 2
+o.expandtab = true
+
+o.smartindent = true
 o.autoindent = true
 
-opt.wrap = false
+o.wrap = false
 
 o.ignorecase = true -- ignore case when searching
 o.smartcase = true --unless you include mixed case in your search
 
-opt.termguicolors = true
-opt.background = "dark" -- colorschemes that can be light or dark will be dark
+o.termguicolors = true
+o.background = "dark" -- colorschemes that can be light or dark will be dark
 o.signcolumn = "yes" -- show sign column so that text doesn't shift
-
 
 o.splitright = true
 o.splitbelow = true
 
-opt.hlsearch = true
+o.hlsearch = false
+o.incsearch = true
+
+o.scrolloff = 8
+o.colorcolumn = "100"
